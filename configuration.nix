b926192b8c7bd6ee04c8165c0e8df73d2523db8b
b4lisong <b4lisong@pm.me>
2024-07-22 15:40:50 -0700

    adding vim/neovim base config

--- configuration.nix
+++ configuration.nix
@@ -139,6 +139,151 @@ home-manager.users.balisong = { pkgs, ... }: {
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
+      configure = {
+          customRC = ''
+          """ Vim/Neovim config | ref: https://github.com/amix/vimrc/blob/master/vimrcs/basic.vim """
+          """ => General """
+          " Sets how many lines of history VIM has to remember
+          set history=500
+          
+          " Enable filetype plugins
+          filetype plugin on
+          filetype indent on
+          
+          " Set to auto read when a file is changed from the outside
+          set autoread
+          au FocusGained,BufEnter * silent! checktime
+          
+          " With a map leader it's possible to do extra key combinations
+          " like <leader>w saves the current file
+          let mapleader = ","
+          
+          " Fast saving
+          nmap <leader>w :w!<cr>
+          
+          " :W sudo saves the file
+          " (useful for handling the permission-denied error)
+          command! W execute 'w !sudo tee % > /dev/null' <bar> edit!
+          
+          """ => UI """
+		  " Set 7 lines to the cursor - when moving vertically using j/k
+          set so=7
+          
+          " Turn on the Wild menu
+          set wildmenu
+          
+          " Ignore compiled files
+          set wildignore=*.o,*~,*.pyc
+          if has("win16") || has("win32")
+              set wildignore+=.git\*,.hg\*,.svn\*
+          else
+              set wildignore+=*/.git/*,*/.hg/*,*/.svn/*,*/.DS_Store
+          endif
+          
+          " Always show current position
+          set ruler
+          
+          " Height of the command bar
+          set cmdheight=1
+          
+          " A buffer becomes hidden when it is abandoned
+          set hid
+          
+          " Configure backspace so it acts as it should act
+          set backspace=eol,start,indent
+          set whichwrap+=<,>,h,l
+          
+          " Ignore case when searching
+          set ignorecase
+          
+          " When searching try to be smart about cases
+          set smartcase
+          
+          " Highlight search results
+          " set hlsearch
+          
+          " Makes search act like search in modern browsers
+          set incsearch
+          
+          " Don't redraw while executing macros (good performance config)
+          set lazyredraw
+          
+          " For regular expressions turn magic on
+          set magic
+          
+          " Show matching brackets when text indicator is over them
+          set showmatch
+          
+          " How many tenths of a second to blink when matching brackets
+          set mat=2
+          
+          " No annoying sound on errors
+          set noerrorbells
+          set novisualbell
+          set t_vb=
+          set tm=500
+          
+          " Add a bit extra margin to the left
+          set foldcolumn=1
+          
+          " show absolute number (current line)
+          set number
+          " show relative number (surrounding lines)
+          set relativenumber
+
+	      """ => Text, tab, and indent related """
+          " Use spaces instead of tabs
+          set expandtab
+          
+          " Be smart when using tabs ;)
+          set smarttab
+          
+          " 1 tab == 4 spaces
+          set shiftwidth=4
+          set tabstop=4
+          
+          " Linebreak on 500 characters
+          set lbr
+          set tw=500
+          
+          set ai "Auto indent
+          set si "Smart indent
+          set wrap "Wrap lines
+          
+          """ => Keybinds """
+          " esc in insert & visual mode
+          inoremap jk <esc>
+          vnoremap jk <esc>
+          
+          " esc in command mode
+          cnoremap jk <C-C>
+          " Note: In command mode mappings to esc run the command for some odd
+          " historical vi compatibility reason. We use the alternate method of
+          " existing which is Ctrl-C
+          
+          """ => Colors and Fonts """
+          " Enable syntax highlighting
+          syntax enable
+          
+          " Set regular expression engine automatically
+          set regexpengine=0
+          
+          set background=dark
+          
+          " Set utf8 as standard encoding and en_US as the standard language
+          set encoding=utf8
+          
+          " Use Unix as the standard file type
+          set ffs=unix,dos,mac
+
+          """ => Files, backups, and undo """
+          " Turn backup off, since most stuff is in SVN, git etc. anyway...
+          set nobackup
+          set nowb
+          set noswapfile
+
+	  '';
+      };
   };
 
   # Starship prompt
